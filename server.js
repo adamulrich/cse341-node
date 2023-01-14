b8061ae026b6dd4f4ec5fc3161db28b31d569b41
Adam Ulrich <adamulrich@hotmail.com>
2023-01-13 16:16:39 -0800

    test

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
+
 require('dotenv').config();
 const port = process.env.PORT
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
 const connectionString = process.env.MONGO_CONNECT_STRING;
 
-const mongoDB = new MongoClient(connectionString); 
-
-
-
+const mongoDB = require('./dbconnect');
+mongoDB.initDB();
 
 app.use('/', require('./routes/contacts'))
 app.use('/',require('./routes/index'))
