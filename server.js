3ae3eb99b3ff89f3c7e9df4aca71d1a0d46a96f7
Adam Ulrich <adamulrich@hotmail.com>
2023-01-24 19:46:12 -0800

    cors change

--- server.js
+++ server.js
@@ -8,26 +8,6 @@ const swaggerUI = require("swagger-ui-express");
 const mongoDB = require('./dbconnect');
 mongoDB.initDB();
 
-const options = {
-    definition: {
-        openapi : "3.0.0",
-        info : {
-            title: "Contacts Project for CSE 341",
-            version: "1.0.0",
-        },
-        servers: [
-            {
-            url: "http://localhost:3000"
-            }
-        ]
-    },
-    apis: ["./routes/contacts.js", 
-            "./routes/index.js"
-        ]
-}
-
-
-
 //express
 const express = require('express')
 const app = express();
@@ -36,11 +16,19 @@ const app = express();
 app.use(express.json());
 app.use('/', require('./routes/index'));
 app.use('/contacts', require('./routes/contacts'));
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+})
 // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 
 //start
-app.listen(port, () => {
+app.listen(port, (res, req) => {
+    
     console.log(`App listening on port ${port}`)
 })
             
