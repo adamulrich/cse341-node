bf8cbb12113fdd7b332cde1025f6967177e71fae
Adam Ulrich <adamulrich@hotmail.com>
2023-01-24 20:26:47 -0800

    test

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -95,7 +95,7 @@ async function deleteContact(req, res) {
 function setHeaders(res) {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     res.setHeader('Access-Control-Allow-Credentials', true);
     
